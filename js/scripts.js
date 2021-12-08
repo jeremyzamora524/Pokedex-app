0fd8af88711dd2399fc1adf146a48a2469a944db
jeremyzamora524 <91992997+jeremyzamora524@users.noreply.github.com>
2021-12-08 13:27:43 -0600

    Update scripts.js
    deleted old java script code and replaced it with an array of different pokemon starting with name the type of pokemon they are and the height of the pokemon .

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello World');
 
-favoriteFood= 'Steak';
-document.write(favoriteFood);
+let pokemonList [
+    {name:'balbasur', type:'grass', height: 0.7},
+    {name:'chalmander', type:'fire', height: 0.6},
+    {name:'squirtle', type:'water', height: 0.5}
+];
+
