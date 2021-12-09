026daa77009eb1f4851fa47c544aa29006a90870
jeremyzamora524 <91992997+jeremyzamora524@users.noreply.github.com>
2021-12-08 21:33:04 -0600

    Update scripts.js
    added a for loop for my pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 
-let pokemonList [
+let pokemonList = [
     {name:'balbasur', type:'grass', height: 0.7},
     {name:'chalmander', type:'fire', height: 0.6},
     {name:'squirtle', type:'water', height: 0.5}
 ];
 
+for (let i=0; i < pokemonList.length; i++){
+ document.write('<br>' + pokemonList[i].name + (' ,height:') + pokemonList[i].height);
+ if (pokemonList[i].height >0.6){
+document.write(' -- Big Pokemon');
+ }
+}
