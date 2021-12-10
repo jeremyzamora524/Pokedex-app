0bec337272a2a196d79dd94220c85452764300e7
jeremyzamora524 <91992997+jeremyzamora524@users.noreply.github.com>
2021-12-09 18:44:16 -0600

    Update scripts.js
    IIFE and  forEach were added

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
 
+let pokemonRespository = (function () {
+
 let pokemonList = [
     {name:'balbasur', type:'grass', height: 0.7},
     {name:'chalmander', type:'fire', height: 0.6},
     {name:'squirtle', type:'water', height: 0.5}
 ];
 
-for (let i=0; i < pokemonList.length; i++){
- document.write('<br>' + pokemonList[i].name + (' ,height:') + pokemonList[i].height);
- if (pokemonList[i].height >0.6){
-document.write(' -- Big Pokemon');
- }
-}
+
+function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  
+  
+  function getAll() {
+    return pokemonList;
+  }
+  
+  return {
+    add: add,
+    getAll: getAll
+  };
+  
+  
+  })();
+  
+  
+  pokemonRespository.getAll().forEach(function(pokemon) {
+    document.write('<p>' +  pokemon.name + '   ' + ' Height: ' + pokemon.height + ' Type: ' + pokemon.type + '</p>');
+  });
+  
+
